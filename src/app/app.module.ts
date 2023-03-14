eb2d6cd6b1bc4dbd0c59a557363fe6fd157a9235
Benard Mathu <mathubenard@gmail.com>
2023-03-14 23:04:51 +0300

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "contacts", component: ContactsComponent },
+  { path: "about", component: AboutComponent },
+  { path: "portfolio", component: PortfolioComponent },
+  { path: "**", component: PageNotFoundComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ContactsComponent,
+    AboutComponent,
+    PortfolioComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
