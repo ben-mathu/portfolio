aa4b2b39730659bad00198d7c4ea4a976821a18a
Benard Mathu <mathubenard@gmail.com>
2023-11-21 09:55:54 +0300

    animaton links to sites

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,3 @@
-import { gsap } from 'gsap';
 import { FirebaseService } from './../../shared/services/firebase.service';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MyDetails } from 'src/app/shared/models/header/header';
@@ -9,24 +8,10 @@ import { MyDetails } from 'src/app/shared/models/header/header';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  @ViewChild('menu') element: ElementRef | undefined;
 
   myDetails: MyDetails = new MyDetails();
-  timeline: gsap.core.Timeline;
 
   constructor(private service: FirebaseService) {
-    this.timeline = gsap.timeline()
-      .fromTo(this.element!, {
-        opacity: 0,
-        translateY: -30
-      },
-      {
-        duration: 1,
-        opacity: 1,
-        translateY: 0,
-        stagger: 0.2
-      }
-      )
   }
 
   ngOnInit(): void {
