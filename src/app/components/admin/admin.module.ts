6c676aebe3ff972cbb345b41f3dc86fbfd9ccd86
Benard Mathu <mathubenard@gmail.com>
2024-08-13 07:35:06 +0300

    fixes login form ui

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -14,6 +14,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { SignupComponent } from './signup/signup.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatCheckboxModule,
     ReactiveFormsModule,
     provideAuth(() => getAuth()),
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule
   ],
   providers: [
     AuthService,
