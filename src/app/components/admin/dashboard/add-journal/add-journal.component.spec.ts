1f29031b6e8d4403090ea9b2bccb8121a36f9f09
Benatt <mathubenard@gmail.com>
2025-09-21 17:06:33 +0300

    fixes add journal tests deps

--- src/app/components/admin/dashboard/add-journal/add-journal.component.spec.ts
+++ src/app/components/admin/dashboard/add-journal/add-journal.component.spec.ts
@@ -2,8 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddJournalComponent } from './add-journal.component';
 import { FirebaseService } from 'src/app/shared/services/firebase/firebase.service';
-import { Utils } from 'src/app/shared/utils/utils';
 import { RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { SharedModule } from 'src/app/shared/components/shared.module';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatInputModule } from '@angular/material/input';
+import { MarkdownModule } from 'ngx-markdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AddJournalComponent', () => {
   let component: AddJournalComponent;
@@ -11,19 +17,29 @@ describe('AddJournalComponent', () => {
 
   beforeEach(async () => {
     let mockFirebaseService = jasmine.createSpyObj('FirebaseService', [
-      'updateJournal', 'saveJournal', 'deleteBlog'
+      'updateJournal',
+      'saveJournal',
+      'deleteBlog',
     ]);
     await TestBed.configureTestingModule({
-      imports: [ RouterModule.forRoot([]) ],
+      imports: [
+        RouterModule.forRoot([]),
+        MatFormFieldModule,
+        SharedModule,
+        MatChipsModule,
+        MatInputModule,
+        MarkdownModule.forRoot(),
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+      ],
       declarations: [AddJournalComponent],
       providers: [
         {
           provide: FirebaseService,
-          useValue: mockFirebaseService
-        }
-      ]
-    })
-    .compileComponents();
+          useValue: mockFirebaseService,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AddJournalComponent);
     component = fixture.componentInstance;
