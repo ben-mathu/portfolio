e8cbc54ec52ccd254123a1f52af04b60f4d20b1e
Benatt <mathubenard@gmail.com>
2025-09-21 16:47:57 +0300

    fixes footer component test dependencies

--- src/app/shared/components/footer/footer.component.spec.ts
+++ src/app/shared/components/footer/footer.component.spec.ts
@@ -2,6 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
 import { Utils } from '../../utils/utils';
+import { SharedModule } from '../shared.module';
+import { SocialIconsComponent } from '../social-icons/social-icons.component';
+import { MatListModule } from '@angular/material/list';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -9,10 +12,10 @@ describe('FooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ],
-      providers: [ Utils ]
-    })
-    .compileComponents();
+      imports: [MatListModule, SharedModule],
+      declarations: [FooterComponent],
+      providers: [Utils],
+    }).compileComponents();
   });
 
   beforeEach(() => {
