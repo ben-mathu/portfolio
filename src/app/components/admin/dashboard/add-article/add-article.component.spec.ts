c78347c84ebfe54aa86231aaf6ee9be15d8fffa1
Benatt <mathubenard@gmail.com>
2025-09-21 16:57:09 +0300

    fixes add article tests deps

--- src/app/components/admin/dashboard/add-article/add-article.component.spec.ts
+++ src/app/components/admin/dashboard/add-article/add-article.component.spec.ts
@@ -2,26 +2,40 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddArticleComponent } from './add-article.component';
 import { FirebaseService } from 'src/app/shared/services/firebase/firebase.service';
-import { Utils } from 'src/app/shared/utils/utils';
 import { RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { SharedModule } from 'src/app/shared/components/shared.module';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatInputModule } from '@angular/material/input';
+import { MarkdownModule } from 'ngx-markdown';
 
 describe('AddArticleComponent', () => {
   let component: AddArticleComponent;
   let fixture: ComponentFixture<AddArticleComponent>;
 
   beforeEach(async () => {
-    let mockFirebaseService = jasmine.createSpyObj('FirebaseService', ['updateBlog', 'saveBlog', 'deleteBlog']);
+    let mockFirebaseService = jasmine.createSpyObj('FirebaseService', [
+      'updateBlog',
+      'saveBlog',
+      'deleteBlog',
+    ]);
     await TestBed.configureTestingModule({
-      imports: [ RouterModule.forRoot([]) ],
+      imports: [
+        RouterModule.forRoot([]),
+        MatFormFieldModule,
+        SharedModule,
+        MatChipsModule,
+        MatInputModule,
+        MarkdownModule.forRoot(),
+      ],
       declarations: [AddArticleComponent],
       providers: [
         {
           provide: FirebaseService,
-          useValue: mockFirebaseService
-        }
-      ]
-    })
-    .compileComponents();
+          useValue: mockFirebaseService,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AddArticleComponent);
     component = fixture.componentInstance;
